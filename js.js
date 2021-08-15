ffe10dee406bc4b93e0dcba28f2c1e3f571bb863
DucThanhPR01 <Ducvyathanh@gmail.com>
2021-08-15 10:18:08 +0700

    update

---
+++ js.js
@@ -0,0 +1,17 @@
+﻿ $(function(){
+  //click vao nut nao thi cho no doi class
+  $('.bonnut li').click(function(){
+    $('.bonnut li').removeClass('active');
+    $(this).addClass('active');
+
+  var x= $(this).index();
+      x=x+1;
+  
+    // console.log("Vi tri cua phan tu duoc click la: " + x)
+    $('.bonnd ul li').removeClass('hienlen');
+    $('.bonnd ul li:nth-child('+x+')').addClass('hienlen');
+
+  })
+})  
+
+
